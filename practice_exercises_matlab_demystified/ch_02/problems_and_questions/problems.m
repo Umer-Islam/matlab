2faca909e60201e043a2bd1385f0f400d121f5e4
Umer-Islam <139012668+Umer-Islam@users.noreply.github.com>
2024-04-03 06:41:23 +0500

    add problem

---
+++ practice_exercises_matlab_demystified/ch_02/problems_and_questions/problems.m
@@ -0,0 +1,11 @@
+%%
+clc,clearvars;
+% lets say you need 10 equal spaced values of exp(x)
+
+x_data_points = 0:1:10 % output should be [ 0     1     2     3     4     5     6     7     8     9    10 ]
+format long
+exp_x_data_points = exp(x_data_points)
+
+%%
+clc,clearvars;
+
